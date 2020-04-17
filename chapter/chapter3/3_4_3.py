843cbf60b7aac225243954371305f4df8071ece7
ron <759782075@qq.com>
2020-04-17 13:27:07 +0800

    x4/17

---
+++ chapter/chapter3/3_4_3.py
@@ -0,0 +1,66 @@
+# reshape(-1)变为一维张量
+# A.shape=(3,4)
+# A.reshape(-1,6)=A.reshape(2,6)
+
+from keras.datasets import imdb
+(train_data,train_labels),(test_data,test_labels)=imdb.load_data(num_words=10000)
+word_index=imdb.get_word_index()
+reverse_word_index=dict([(value,key) for (key,value) in word_index.items()])
+decoded_review=''.join([reverse_word_index.get(i-3,'?')for i in train_data[0]])
+
+import numpy as np
+
+def vectorize_sequences(sequences,dimension=10000):
+    results=np.zeros((len(sequences),dimension))
+    for i, sequence in enumerate(sequences):
+        results[i,sequence]=1.
+    return results
+
+x_train=vectorize_sequences(train_data)
+x_test=vectorize_sequences(test_data)
+
+y_train=np.asarray(train_labels).astype('float32')
+y_test=np.asarray(test_labels).astype('float32')
+
+
+from keras import models
+from keras import layers
+model=models.Sequential()
+model.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+
+from keras import optimizers
+from keras import losses
+from keras import metrics
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),loss=losses.binary_crossentropy,metrics=[metrics.binary_accuracy])
+#model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
+
+#割训练资料
+x_val=x_train[:10000]
+partial_x_train=x_train[10000:]
+
+y_val=y_train[:10000]
+partial_y_train=y_train[10000:]
+
+history=model.fit(partial_x_train,partial_y_train,epochs=20,batch_size=512,validation_data=(x_val,y_val))
+
+history_dict=history.history
+history_dict.keys()
+
+
+prediction=model.predict_classes(x_test)
+
+import pandas as pd
+a56=pd.crosstab(y_test,prediction.reshape(-1),rownames=['label'],colnames=['predict'])
+print(a56)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test,prediction))
+
+import copy
+test_copy=copy.copy(y_test)
+np.random.shuffle(test_copy)
+a64=float(np.sum(np.array(y_test) == np.array(test_copy)))/len(y_test)
+print(a64)
